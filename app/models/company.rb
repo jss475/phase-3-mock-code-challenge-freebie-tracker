80cf9a6e6f1efbc99f283478c54a35c19f125d6e
JosephShin <joespam9999@gmail.com>
2022-05-24 11:37:38 -0400

    commit tree 282d86d88aac447ff580a2604211f9fc41fd7aa0

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,23 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def freebies
+        Freebie.where("company_id = ?",self.id)
+    end
+
+    def devs
+        all_freebies = self.freebies
+        all_devs = all_freebies.map do |freebie|
+            Dev.where("id = ?",freebie.dev_id).first
+        end
+    end
+
+    def give_freebie(dev, item_name, value)
+        Freebie.create(dev_id: dev.id, item_name: item_name, value: value, company_id: self.id)
+    end
+
+    def self.oldest_company
+        Company.all.order(:founding_year).first
+    end
 end
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Freebie < ActiveRecord::Base
+    belongs_to :dev
+    belongs_to :company
+    def dev
+        Dev.where("id = ?", self.dev_id).first
+    end
+
+    def company
+        Company.where("id = ?", self.company_id).first
+    end
+
+    def print_details
+        "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+    end
 
 end
